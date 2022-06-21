8a67ead9320fb1272425055a082837458e663c3e
warmkatahira <t.katahira@warm.co.jp>
2022-06-21 09:04:21 +0900

    demo title grid add 2

--- resources/views/data/top.blade.php
+++ resources/views/data/top.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <span class="font-semibold text-lg text-gray-800">
-            データ
-        </span>
+        <div class="grid grid-cols-12">
+            <span class="font-semibold text-lg text-gray-800 col-span-8 py-3">
+                データ
+            </span>
+        </div>
     </x-slot>
     <div class="py-2">
         <div class="mx-5">
